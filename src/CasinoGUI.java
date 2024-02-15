837f2b51d5b4caf74fd350a471ed66a8f8ec548b
tmb5932 <tmb5932@g.rit.edu>
2024-02-15 12:33:50 -0500

    Made updates to the GUI for signing up and logging in.

--- src/CasinoGUI.java
+++ src/CasinoGUI.java
@@ -4,6 +4,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -15,9 +16,10 @@ import javafx.stage.Stage;
 public class CasinoGUI extends Application implements Observer<CasinoModel, String> {
     private CasinoModel model;
     private final static String RESOURCES_DIR = "resources/";
-    private Label topLabel;
+    private Label topLabel = new Label("Please Sign Up or Login");
     private Button[][] buttonArray;
-    private Label message = new Label();
+    private Label loginMessage = new Label("Login");
+    private Label signupMessage = new Label("Sign up");
     private final Font basicFont = new Font("Ariel", 19);
     private Stage mainStage;
     private Scene mainScene;
@@ -41,52 +43,51 @@ public class CasinoGUI extends Application implements Observer<CasinoModel, Stri
     public void start(Stage stage) {
         mainStage = stage;
         buttonArray = new Button[2][3];
-        TextField usernameField1 = new TextField();
-        TextField passwordField1 = new TextField();
-        TextField usernameField2 = new TextField();
-        TextField passwordField2 = new TextField();
-
-        GridPane signupGrid = new GridPane();
-        signupGrid.setPadding(new Insets(10, 10, 10, 10));
-        signupGrid.setVgap(5);
-        signupGrid.setHgap(5);
-
-        GridPane loginGrid = new GridPane();
-        loginGrid.setPadding(new Insets(10, 10, 10, 10));
-        loginGrid.setVgap(5);
-        loginGrid.setHgap(5);
+        TextField loginUsernameField = new TextField();
+        PasswordField loginPasswordField = new PasswordField();
+        TextField signupUsernameField = new TextField();
+        PasswordField signupPasswordField = new PasswordField();
 
         // LABEL SECTION
-        topLabel = new Label("Please Sign Up or Login");
         topLabel.setFont(basicFont);
         topLabel.setAlignment(Pos.TOP_CENTER);
-        usernameField1.setPromptText("Enter your Username");
-        passwordField1.setPromptText("Enter your Password");
-        usernameField1.setFocusTraversable(false);
-        passwordField1.setFocusTraversable(false);
-        usernameField2.setPromptText("Enter your Username");
-        passwordField2.setPromptText("Enter your Password");
-        usernameField2.setFocusTraversable(false);
-        passwordField2.setFocusTraversable(false);
+        signupMessage.setFont(basicFont);
+        signupMessage.setAlignment(Pos.TOP_CENTER);
+        loginMessage.setFont(basicFont);
+        loginMessage.setAlignment(Pos.TOP_CENTER);
+        loginUsernameField.setPromptText("Enter your Username");
+        loginPasswordField.setPromptText("Enter your Password");
+        loginUsernameField.setAlignment(Pos.CENTER);
+        loginPasswordField.setAlignment(Pos.CENTER);
+//        loginUsernameField.setFocusTraversable(false); // TODO: Decide if i want this on or off
+//        loginPasswordField.setFocusTraversable(false); // TODO: Decide if i want this on or off
+        signupUsernameField.setPromptText("Enter your Username");
+        signupPasswordField.setPromptText("Enter your Password");
+        signupUsernameField.setAlignment(Pos.CENTER);
+        signupPasswordField.setAlignment(Pos.CENTER);
+//        signupUsernameField.setFocusTraversable(false); // TODO: Decide if i want this on or off
+//        signupPasswordField.setFocusTraversable(false); // TODO: Decide if i want this on or off
 
-        signupScene = new Scene(signupGrid);
 
         // BUTTON SECTION
         Button signUpButton = new Button("Sign up");
         signUpButton.setMinSize(200, 150);
         signUpButton.setFont(basicFont);
-        signUpButton.setAlignment(Pos.CENTER_LEFT);
+        signUpButton.setAlignment(Pos.CENTER);
         signUpButton.setTextAlignment(TextAlignment.CENTER);
         signUpButton.setOnAction(event -> model.setScene(Scenes.SIGNUP));
-
-        loginScene = new Scene(loginGrid);
+        signUpButton.setFocusTraversable(false);
 
         Button logInButton = new Button("Login");
         logInButton.setMinSize(200, 150);
         logInButton.setFont(basicFont);
-        logInButton.setAlignment(Pos.CENTER_RIGHT);
+        logInButton.setAlignment(Pos.CENTER);
         logInButton.setTextAlignment(TextAlignment.CENTER);
-        logInButton.setOnAction(event -> model.setScene(Scenes.LOGIN));
+        logInButton.setOnAction(event -> {
+            model.setScene(Scenes.LOGIN);
+            System.out.println(loginPasswordField.getStyle());
+        });
+        logInButton.setFocusTraversable(false);
 
         Button signupBackButton = new Button("Back");
         signupBackButton.setMinSize(125, 50);
@@ -100,55 +101,53 @@ public class CasinoGUI extends Application implements Observer<CasinoModel, Stri
         loginBackButton.setAlignment(Pos.CENTER);
         loginBackButton.setTextAlignment(TextAlignment.CENTER);
 
+        // Create a new account screen
+        Button signupSubmitButton = new Button("Submit");
+        signupSubmitButton.setMinSize(125, 50);
+        signupSubmitButton.setFont(basicFont);
+        signupSubmitButton.setAlignment(Pos.CENTER);
+        VBox signupVBox = new VBox(signupMessage, signupUsernameField, signupPasswordField, signupSubmitButton, signupBackButton);
+        signupSubmitButton.setOnAction(e -> {
+            if ((signupUsernameField.getText() != null && !signupPasswordField.getText().isEmpty())) {
+                signupMessage.setText(signupUsernameField.getText() + ", thank you for signing up!");
+                model.signUp(signupUsernameField.getText(), signupPasswordField.getText());
+            } else {
+                signupMessage.setText("You have not entered the required fields.");
+            }
+        });
+        signupVBox.setSpacing(10);
+        signupVBox.setPadding(new Insets(10, 50, 10, 50));
+        signupVBox.setAlignment(Pos.CENTER);
+        signupScene = new Scene(signupVBox, 480, 244);
+
+        // Log into a previously existing account screen
+        Button loginSubmitButton = new Button("Submit");
+        loginSubmitButton.setMinSize(125, 50);
+        loginSubmitButton.setFont(basicFont);
+        loginSubmitButton.setAlignment(Pos.CENTER);
+        VBox loginVBox = new VBox(loginMessage, loginUsernameField, loginPasswordField, loginSubmitButton, loginBackButton);
+        loginSubmitButton.setOnAction(e -> {
+            if ((loginUsernameField.getText() != null && !loginPasswordField.getText().isEmpty())) {
+                loginMessage.setText(loginUsernameField.getText() + ", thank you for logging in!");
+                model.login(loginUsernameField.getText(), loginPasswordField.getText());
+            } else {
+                loginMessage.setText("You have not entered the required fields.");
+            }
+        });
+        loginVBox.setSpacing(10);
+        loginVBox.setPadding(new Insets(10, 50, 10, 50));
+        loginVBox.setAlignment(Pos.CENTER);
+        loginScene = new Scene(loginVBox, 480, 244);
 
         HBox startScreenHBox = new HBox(signUpButton, logInButton);
         startScreenHBox.setAlignment(Pos.CENTER);
+        startScreenHBox.setSpacing(10);
+
         VBox startScreenVBox = new VBox(topLabel, startScreenHBox);
         startScreenVBox.setAlignment(Pos.CENTER);
 
-        // Log into a previously existing account screen
-        Button submitLoginButton = new Button("Submit");
-        submitLoginButton.setMinSize(125, 50);
-        submitLoginButton.setFont(basicFont);
-        submitLoginButton.setAlignment(Pos.CENTER);
-        GridPane.setConstraints(usernameField1, 0, 0);
-        loginGrid.getChildren().add(usernameField1);
-        GridPane.setConstraints(passwordField1, 0, 1);
-        loginGrid.getChildren().add(passwordField1);
-        GridPane.setConstraints(submitLoginButton, 0, 3);
-        loginGrid.getChildren().add(submitLoginButton);
-        GridPane.setConstraints(loginBackButton, 0, 4);
-        loginGrid.getChildren().add(loginBackButton);
-        submitLoginButton.setOnAction(e -> {
-            if ((usernameField1.getText() != null && !passwordField1.getText().isEmpty())) {
-                topLabel.setText(usernameField1.getText() + ", thank you for logging in!");
-                model.login(usernameField1.getText(), passwordField1.getText());
-            } else {
-                topLabel.setText("You have not entered the required fields.");
-            }
-        });
-
-        // Create a new account screen
-        Button submitSignupButton = new Button("Submit");
-        submitSignupButton.setMinSize(125, 50);
-        submitSignupButton.setFont(basicFont);
-        submitSignupButton.setAlignment(Pos.CENTER);
-        GridPane.setConstraints(usernameField2, 0, 0);
-        signupGrid.getChildren().add(usernameField2);
-        GridPane.setConstraints(passwordField2, 0, 1);
-        signupGrid.getChildren().add(passwordField2);
-        GridPane.setConstraints(submitSignupButton, 0, 3);
-        signupGrid.getChildren().add(submitSignupButton);
-        GridPane.setConstraints(signupBackButton, 0, 4);
-        signupGrid.getChildren().add(signupBackButton);
-        submitSignupButton.setOnAction(e -> {
-            if ((usernameField2.getText() != null && !passwordField2.getText().isEmpty())) {
-                topLabel.setText(usernameField2.getText() + ", thank you for signing up!");
-                model.signUp(usernameField2.getText(), passwordField2.getText());
-            } else {
-                topLabel.setText("You have not entered the required fields.");
-            }
-        });
+        startScreenVBox.setSpacing(30);
+        startScreenVBox.setPadding(new Insets(20, 35, 20, 35));
 
         mainScene = new Scene(startScreenVBox);
         signupBackButton.setOnAction(event -> model.setScene(Scenes.HOMEPAGE));
@@ -165,11 +164,23 @@ public class CasinoGUI extends Application implements Observer<CasinoModel, Stri
         model = casinoModel;
         switch (casinoModel.currentScene) {
             case LOGIN -> mainStage.setScene(loginScene);
+
             case SIGNUP -> mainStage.setScene(signupScene);
+
             case HOMEPAGE -> mainStage.setScene(mainScene);
+
             case GAMESTAGE -> mainStage.setScene(gamesScene);
         }
-        topLabel.setText(text);
-        message.setText(text);
+        if (text != null) {
+            switch (casinoModel.currentScene) {
+                case LOGIN ->  loginMessage.setText(text);
+
+                case SIGNUP -> signupMessage.setText(text);
+
+                case HOMEPAGE -> topLabel.setText(text);
+
+                case GAMESTAGE -> topLabel.setText(text);
+            }
+        }
     }
 }
